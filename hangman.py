966cf7e9c2a2a2f6d8d79e50b4c2137d2596ed89
Precious Onu <arnoldsynchron@gmail.com>
2020-07-15 00:27:27 +0100

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -1,7 +1,7 @@
 # Problem Set 2, hangman.py
-# Name: 
-# Collaborators:
-# Time spent:
+# Name: Precious Onu
+# Collaborators: Self
+# Time spent: 20 hours
 
 # Hangman Game
 # -----------------------------------
